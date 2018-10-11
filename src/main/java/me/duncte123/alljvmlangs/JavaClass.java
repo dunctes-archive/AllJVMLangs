313a1e3b6f109fb0c154dd4ed45054452fe60b02
Duncan Sterken <duncan.sterken123@gmail.com>
2018-10-11 13:46:39 +0200

    Java was still set to sout

--- src/main/java/me/duncte123/alljvmlangs/JavaClass.java
+++ src/main/java/me/duncte123/alljvmlangs/JavaClass.java
@@ -1,8 +1,10 @@
 package me.duncte123.alljvmlangs;
 
+import static me.duncte123.alljvmlangs.MessageHandler.printMln;
+
 public class JavaClass implements IBase {
     @Override
     public void printMessage() {
-        System.out.println("Hello from java");
+        printMln("Hello from java");
     }
 }
